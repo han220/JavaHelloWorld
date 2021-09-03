c81049000189b3c72a5b47f851172716661ae23e
han220 <22000462@handong.edu>
2021-09-03 21:25:16 +0900

    Remove todo and close stream

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		System.out.print("이름 입력 : ");
 		String name = s.next();
 		System.out.println("["+name+"]님 환영합니다~");
+		s.close();
 	}
 
 }
